9b345f251655a074010496773d9677440156c9a5
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-09-02 11:54:46 +0530

    connect to database Mysql connection

--- currency-exchange-service/src/main/java/com/dgsh/microservices/currencyexchangeservice/bean/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/dgsh/microservices/currencyexchangeservice/bean/CurrencyExchange.java
@@ -2,9 +2,19 @@ package com.dgsh.microservices.currencyexchangeservice.bean;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
 public class CurrencyExchange {
+	
+	@Id
 	private Long id;
+	@Column(name = "currency_from")
 	private String from;
+	@Column(name = "currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private String environment;
